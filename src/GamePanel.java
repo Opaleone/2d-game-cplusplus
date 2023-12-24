9c0ee6543cd593f7d5b307e11e328d5872a3425f
Leone Varando <lvarando@gmail.com>
2023-12-23 18:34:53 -0600

    Created game panel and created beginning of game loop

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,40 @@
 package src;
 
-public class GamePanel {
-  
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class GamePanel extends JPanel implements Runnable {
+  // Sets size and scale to create panel
+  final int originalTileSize = 16;
+  final int scale = 3;
+
+  // Finds actual size
+  final int tileSize = originalTileSize * scale;
+  final int maxScreenCol = 16;
+  final int maxScreenRow = 12;
+  final int screenWidth = tileSize * maxScreenCol;
+  final int screenHeight = tileSize * maxScreenRow;
+
+  Thread gameThread;
+
+  // Creating game panel
+  public GamePanel() {
+    this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+    this.setBackground(Color.black);
+    this.setDoubleBuffered(true);
+  }
+
+  public void startGameThread() {
+    gameThread = new Thread(this);
+    gameThread.start();
+  }
+
+  // Automatically called when gameThread is called
+  public void run() {
+    while(gameThread != null) {
+      System.out.println("The game loop is running!");
+    }
+  }
 }
