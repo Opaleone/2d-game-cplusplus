289b1265a774c6c0c0129751077995c0d7c0ca89
Leone Varando <lvarando@gmail.com>
2023-12-23 18:35:51 -0600

    Set gamePanel to jframe and started gamethread

--- Main.java
+++ Main.java
@@ -1,7 +1,24 @@
+import javax.swing.JFrame;
+
 import src.*;
 
 public class Main {
   public static void main(String[] args) {
-    
+    JFrame window = new JFrame();
+    // Allows us to close
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // Can be set to false to restrict panel to pre-designatd size
+    window.setResizable(true);
+    window.setTitle("2d game Tutorial");
+
+    GamePanel gamePanel = new GamePanel();
+    window.add(gamePanel);
+
+    window.pack();
+
+    window.setLocationRelativeTo(null);
+    window.setVisible(true);
+
+    gamePanel.startGameThread();
   }
 }
