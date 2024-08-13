a31037aa90cd83d605c34e7ab016d8edfff8609b
Leone Varando <lvarando@gmail.com>
2024-08-13 09:09:00 -0500

    Added tile image and draw

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -1,7 +1,10 @@
 package src.tile;
 
-import java.awt.Color;
+// import java.awt.Color;
 import java.awt.Graphics2D;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import src.main.GamePanel;
 
@@ -11,16 +14,34 @@ public class TileManager {
 
   public TileManager(GamePanel gp) {
     this.gp = gp;
+    tile = new Tile[1];
+    getTileImage();
+  }
+
+  public void getTileImage() {
+    try {
+      tile[0] = new Tile();
+      tile[0].image = ImageIO.read(getClass().getResourceAsStream("/res/tiles/Floor1.png"));
+    } catch(IOException e) {
+      e.printStackTrace();
+    }
   }
 
   public void draw(Graphics2D g2) {
+    // draw straight white line across bottom of frame
     int x = 0;
-    int y = gp.screenHeight - 40;
+    int y = gp.screenHeight - 42;
     
-    for (int i = 0; i < gp.screenWidth; i++) {
-      g2.setColor(Color.white);
-      g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+    for (int i = 0; i < (gp.screenWidth / gp.tileSize + 1); i++) {
+      if (i == 10 || i == 11 || i == 12 || i == 13) {
+        x += gp.tileSize;
+        continue;
+      }
+
+      g2.drawImage(tile[0].image, x, y, gp.tileSize, gp.tileSize, null);
       x += gp.tileSize;
+
+      System.out.println(i);
     }
   }
 }
