d7d3fa25705c72e85f805209b27957a92e943fae
Leone Varando <lvarando@gmail.com>
2024-08-16 11:44:19 -0500

    Added width and height

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -4,7 +4,7 @@ import java.awt.Rectangle;
 
 public class Entity {
 
-  public int x, y;
+  public int x, y, width, height;
   public int speed;
   // set area for collision
   public Rectangle colArea;
