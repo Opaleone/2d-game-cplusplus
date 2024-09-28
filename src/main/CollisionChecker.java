feaf4dd92ecbb3015349faee622a9539fa90e121
Leone Varando <lvarando@gmail.com>
2024-09-28 01:31:38 -0500

    Removed unnecessary system prints

--- src/main/CollisionChecker.java
+++ src/main/CollisionChecker.java
@@ -13,8 +13,6 @@ public class CollisionChecker {
   public static boolean isColliding(Player player, Rectangle tile) {
     Rectangle pRect = player.getBounds();
     Rectangle tRect = tile.getBounds();
-    // System.out.println("Player: " + pRect);
-    // System.out.println("Tile: " + tRect);
 
     if (pRect.intersects(tRect)) {
       return true;
