a9dd8add401fca4076378a134cf6a9dfa00d3677
Leone Varando <lvarando@gmail.com>
2024-09-28 01:23:54 -0500

    Added jumpspeed

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -5,7 +5,7 @@ import java.awt.Rectangle;
 public class Entity {
 
   public int x, y, width, height;
-  public int speed;
+  public int speed, jumpSpeed;
   // set area for collision
   public Rectangle colArea;
   public boolean colOn = true;
